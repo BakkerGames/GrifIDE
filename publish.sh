801d23e451c8509c2f4487f673968ec24605f32d
Scott Bakker <sbakker343@gmail.com>
2025-12-05 00:06:32 -0500

    publish.sh

--- publish.sh
+++ publish.sh
@@ -1,1 +1,1 @@
-dotnet publish GrifIDE/GrifIDE.csproj -o /c/Temp --sc true -r win-x64 -p:PublishSingleFile=true
+dotnet publish GrifIDE/GrifIDE.csproj -o /c/Users/sbakk/EXE --sc true -r win-x64 -p:PublishSingleFile=true
